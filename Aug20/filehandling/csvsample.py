37e29ca6335c5af0073b92a02cd68dce174e2049
qtkhaja <qtkhaja@gmail.com>
2020-09-28 19:14:36 +0530

    updated read methods

--- Aug20/filehandling/csvsample.py
+++ Aug20/filehandling/csvsample.py
@@ -11,6 +11,24 @@ def default_menu_items():
     menu_items['idly'] = 40
     return menu_items
 
+def print_file_contents(filepath):
+    """
+    This method will read the contents line by line
+    """
+    file_object = open(file=filepath, mode='rt')
+    for line in file_object:
+        print(line)
+
+
+def read_menu(filepath):
+    """
+    This method will read all the lines and returns them
+    """
+    file_object = open(file=filepath, mode='rt')
+    content = file_object.readlines()
+    file_object.close()
+    return content
+
 if __name__ == "__main__":
     sample_items = default_menu_items()
     menu_file_path = "menu.csv"
@@ -25,5 +43,12 @@ if __name__ == "__main__":
             print(sno,name,price,sep=',',end='\n', file=csv_fileobj)
     finally:
         csv_fileobj.close()
-        
-    
+    print("Contents written are as follows")
+    print()
+    print()
+    print(read_menu(menu_file_path))
+
+    print("Contents written are as follows by using print line by line")
+    print()
+    print()
+    print_file_contents(menu_file_path)
