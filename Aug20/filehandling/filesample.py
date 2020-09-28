d4f2cc891a074520ffa55fc734ac9a39a49bd1ad
qtkhaja <qtkhaja@gmail.com>
2020-09-28 18:42:16 +0530

    used write() method

--- Aug20/filehandling/filesample.py
+++ Aug20/filehandling/filesample.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     file_path = 'message.txt'
     content = "I'm storing text and this is my first file code"
     message_file_object = open(file=file_path, mode='wt')
-    print(content, file=message_file_object)
+    message_file_object.write(content)
     message_file_object.close()
